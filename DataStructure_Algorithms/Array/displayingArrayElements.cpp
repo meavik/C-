d4054fc2b00bddb3545701bd2f0189a548bbaf6f
meaivk <33549886+meaivk@users.noreply.github.com>
2020-07-13 23:27:25 +0600

    Create displayingArrayElements.cpp

---
+++ DataStructure_Algorithms/Array/displayingArrayElements.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void display(struct Array);
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+
+int main(void){
+
+    Array a;
+    cout << "Enter the size of the array: ";
+    cin >> a.size;
+
+    a.A = new int[a.size];
+    a.length = 0;
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a.A[i];
+    }
+    a.length = n;
+    display(a);
+
+    return EXIT_SUCCESS;
+}
+
+void display(struct Array arr){
+
+    cout << "Displaying the array elements: ";
+    for(int i = 0; i < arr.length ; i++){
+
+        cout << arr.A[i] <<" ";
+    }
+    cout<<endl;
+
+}
