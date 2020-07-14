b065d1c2bf333b298f3593f2aed881ab89a1114d
meaivk <33549886+meaivk@users.noreply.github.com>
2020-07-15 01:27:59 +0600

    Create reverseArray().cpp

---
+++ DataStructure_Algorithms/Array/reverseArray().cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+void display(struct Array);
+void reverseArray(struct Array*);
+
+
+struct Array{
+    int *A;
+    int size;
+    int length;
+};
+
+int main(void){
+
+    Array a;
+    cout << "Enter the size of the array: ";
+    cin >> a.size;
+
+    a.A = new int[a.size];
+    a.length = 0;
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin >> n;
+    if(a.size>=n){
+        for(int i = 0; i < n; i++){
+        cin >> a.A[i];
+    }
+    }
+    else{
+        cout<<"Length can't be greater than size."<<endl;
+        return EXIT_SUCCESS;
+    }
+    a.length = n;
+    display(a);
+    reverseArray(&a);
+    cout<<"<-<-<-After reversing->->->"<<endl;
+    display(a);
+
+    return EXIT_SUCCESS;
+}
+
+void display(struct Array arr){
+
+    cout << "Displaying the array elements: ";
+    for(int i = 0; i < arr.length ; i++){
+
+        cout << arr.A[i] <<" ";
+    }
+    cout<<endl;
+
+}
+
+void reverseArray(struct Array* arr){
+//    int temp;
+   for (size_t i = 0,j = arr->length-1; i < j; i++,j--)
+   {
+    //    temp = arr->A[i];
+    //    arr->A[i] = arr->A[j];
+    //    arr->A[j] = temp; 
+    arr->A[i] = arr->A[i]^arr->A[j];
+    arr->A[j] = arr->A[i]^arr->A[j];
+    arr->A[i] = arr->A[i]^arr->A[j];
+   }
+   
+    
+}
