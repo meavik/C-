5e6e404d4c537c95dff7a5828ff1cc5ed0c9426c
meavik <33549886+meavik@users.noreply.github.com>
2020-07-17 19:36:01 +0600

    Create missingMultipleNumber.cpp

---
+++ DataStructure_Algorithms/Array/missingMultipleNumber.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+struct Array{
+    int *A;
+    int length;
+    int size;
+    
+};
+
+int arrayElementInsert(struct Array*,int);
+void arrayElementPrint(const struct Array);
+void missingMultipleNumber(struct Array);
+
+int main(void){
+
+    Array arr;
+    cout<<"Enter the size of the array: ";
+    cin >> arr.size;
+    arr.length = 0;
+    arr.A = new int[arr.size];
+    int rec = arrayElementInsert(&arr,arr.size);
+    if(rec == -1){
+    }
+    else{
+        arrayElementPrint(arr);
+    }
+    missingMultipleNumber(arr);
+
+    delete []arr.A;
+    arr.A = nullptr;
+    return EXIT_SUCCESS;
+}
+
+int arrayElementInsert(struct Array *arr, int size)
+{
+    cout << "How many array elements: ";
+    cin >> arr->length;
+    if (arr->length > arr->size || arr->length < 0)
+    {
+        cout << "Invalid array length" << endl;
+        return -1;
+    }
+    else
+    {
+        cout<<"Enter the array elements: ";
+        for (size_t i = 0; i < arr->length; i++)
+        {
+            cin >> arr->A[i];
+        }
+    }
+    return 0;
+}
+
+void arrayElementPrint(const struct Array arr){
+    cout<<"Displaying the array elements: ";
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        cout<<arr.A[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+void missingMultipleNumber(struct Array arr){
+
+    int diff = arr.A[0] - 0;
+    cout<<"Missing numbers are ";
+    for (size_t i = 0; i < arr.length; i++)
+    {
+        if(arr.A[i]-i != diff){
+            while(diff<arr.A[i]-i){
+                cout<< diff + i<<" ";
+                diff++;
+            }
+        }
+    }
+    cout<<endl;
+    
+    
+}
