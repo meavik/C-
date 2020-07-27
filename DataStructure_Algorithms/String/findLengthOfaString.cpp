140cace8030784e7322906993515c656d7e3c440
meavik <33549886+meavik@users.noreply.github.com>
2020-07-27 23:01:46 +0600

    Create findLengthOfaString.cpp

---
+++ DataStructure_Algorithms/String/findLengthOfaString.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+
+    char *A = "Welcome";
+    size_t i;
+    for (i = 0; A[i] != '\0' ; i++)
+    {
+    }
+
+    cout<<"Length is: "<<i<<endl;
+    
+
+    return EXIT_SUCCESS;
+}
